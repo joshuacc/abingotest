090c41cf570d588704455570eb552ad1f7345ff9
joshuacc <josh@designpepper.com>
2011-08-09 13:54:54 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Abingotest::Application.config.secret_token = 'e8c2f6c72267ed8c811ea7f7654a41a0b8e9c950a9aa4769576ec8a11cc794d5fa8138e0ecc1ffd2265cbb2a871a41301876aa886eb5a07059d2c1f3ab9515bf'
